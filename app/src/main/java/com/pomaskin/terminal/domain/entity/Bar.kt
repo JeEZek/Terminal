6b4ea670a3303af98e0f588ed893b9c893ea9c9b
Kirill <kirill.pomaskin@gmail.com>
2023-11-17 15:23:46 +0200

    Add TerminalState

--- app/src/main/java/com/pomaskin/terminal/domain/entity/Bar.kt
+++ app/src/main/java/com/pomaskin/terminal/domain/entity/Bar.kt
@@ -1,13 +1,14 @@
 package com.pomaskin.terminal.domain.entity
 
 import android.icu.util.Calendar
-import com.google.gson.annotations.SerializedName
-import java.util.Date
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
 
+@Parcelize
 data class Bar(
     val open: Float,
     val close: Float,
     val low: Float,
     val high: Float,
     val time: Calendar
-)
+) : Parcelable
